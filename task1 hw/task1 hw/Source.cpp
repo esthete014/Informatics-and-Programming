807b0d4f7afe8e0a0124663715143c11178d04d3
nikolay kochetov <nikkochetov@outlook.com>
2021-10-08 19:21:08 +0300

    task1 hw(peremennie changed)

--- task1 hw/task1 hw/Source.cpp
+++ task1 hw/task1 hw/Source.cpp
@@ -1,39 +1,51 @@
 #include <stdio.h>  //biblioteka dly vvoda
 #include <math.h>  //biblioteka dly mat deystviy
 
+
 int main() {
 	int kolpolok;
-	float xsm, ysm, zsm, x, y, z;
-	//float dsp, dvp, derevo;
-	float obiemdsp, vesdsp, vesdvp, vesderevo;//dvp i derevo po odnoi detali
-	float zadnyystenka, bokovaystenka, verhnizkrishka, polki, dveri;
-	float vesshkafa;
+	double xsm, ysm, zsm, x, y, z;
+	double obiemdsp, vesdsp, vesdvp, vesderevo;//dvp i derevo po odnoi detali
+	double zadnyystenka, bokovaystenka, verhnizkrishka, polki, dveri;
+	double vesshkafa;
 	printf("napishite cherez probel kaku'u nado visotu(ot 180 do 220sm), shirinu(ot 80 do 120sm) i glubinu(ot 50 do 90sm) shkafa\n");
 	scanf_s("%f %f %f", &ysm, &xsm, &zsm);
 	//perevodim v metri
 	x = xsm / 100;
 	y = ysm / 100;
 	z = zsm / 100;
-	//plotnost' dsp = 685 dvp 1080 derevo = 900
+
+	//plotnost' materialov
+	float dsp, dvp, derevo;
+	dsp = 685;
+	dvp = 1080;
+	derevo = 900;
+
+	//razmeri
+	double mm5, mm15, sm1, mejdupolkmi;
+	mm5 = 0.005;
+	mm15 = 0.015;
+	sm1 = 0.01;
+	mejdupolkmi = 0.4;
 
 	if (ysm > 179 && ysm < 221 && xsm > 79 && xsm < 121 && zsm > 49 && zsm < 91) {
-		zadnyystenka = y * x * 0.0005;//dvp
+		zadnyystenka = y * x * mm5;//dvp
 		//bobkovih stenok dve
-		bokovaystenka = y * z * 0.0015 * 2;//dsp
-		verhnizkrishka = z * x * 0.0015 * 2;//dsp
-		dveri = x * y * 0.001;//derevo
+		bokovaystenka = y * z * mm15 * 2;//dsp
+		verhnizkrishka = z * x * mm15 * 2;//dsp
+		dveri = x * y * sm1;//derevo
 		//kollicestvo polok
-		kolpolok = y / 40;
-		polki = x * z * kolpolok * 0.0015;//dsp
+		kolpolok = y / mejdupolkmi;
+		polki = x * z * kolpolok * mm15;//dsp
 		//tolshina ne bila ukazana, poetomu i vzyl tu, kotoray bila u drugih dsp
 
 		//obiem dsp
 		obiemdsp = bokovaystenka + verhnizkrishka + polki;
 
 		//ves
-		vesdsp = obiemdsp * 685;
-		vesdvp = zadnyystenka * 1080;
-		vesderevo = dveri * 900;
+		vesdsp = obiemdsp * dsp;
+		vesdvp = zadnyystenka * dvp;
+		vesderevo = dveri * derevo;
 
 		vesshkafa = vesdsp + vesdvp + vesderevo;
 		printf("ves shkafa raven %f (kg)", vesshkafa);
---
+++ work in class 8.10.21/work in class 8.10.21/Source.cpp
@@ -0,0 +1,74 @@
+//Copyrigt 2021 esthete014 Nikolay Kochetov
+/*
+изучаем базовые операции над массивами:
+1научиться заполнять массивы
+2заполнять массивы
+3функции над ними:
+3.1произведение нечетных элементов
+3.2произведение элементов с нечетными индексами
+3.3произведение элементов нечетных по значению
+3.4поменять местами элементы с заданными индексами
+3.5поменять местами правую и левую половины местами(два способа)
+3.5.1с  использованием доп массива
+3.5.2без использования доп массива
+
+*/
+
+#include <stdio.h>
+#define SIZE 10
+#include <malloc.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+	//statichnie massivi
+	float massize[SIZE] = { 0 };
+
+
+
+	printf("%f", massize[0]);
+	
+	//dinamicheskie massivi
+	//int* mass;//zvezda eto ukazatel' na nachalo massiva
+	int size;
+	printf("input size of the array: \n>");
+	scanf_s("%d", &size);
+
+	int* mass;//zvezda eto ukazatel' na nachalo massiva
+	mass = (int*)malloc(size * sizeof(int));
+
+	float* mass_f = (float*)malloc(size * sizeof(float));//toje samoe no v odku strochku
+
+	//zapolnenie massiva
+	//luchshe razdelyt' for na inizializatsiu i deistvie
+	//inicializatsiy
+	int i;
+	for (i = 0; i < size; i++) {
+		mass[i] = -5 + rand() % (10 +1);//chislo ot -5 do 5
+
+		
+	}
+	//deistvie
+	i = 0;
+	for (i = 0; i < size; i++) {
+		printf("%d", mass[i]);
+	}
+
+
+
+	//mojno cherez while
+	/*
+	i = 0;
+	while (i < size) {
+		mass[i] = 5;
+
+		i++;
+	}
+	*/
+
+
+	//v konce programmi luchshe chistit' massivi
+	free(mass);
+	free(mass_f);
+	return 0;
+}
